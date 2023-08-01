a5df042a74be897b13109afbcc5a00803fbc5619
Nour-Abu-Foul <nourabufoul@gmail.com>
2023-08-01 16:10:51 +0300

    second commit

--- Task2.py
+++ Task2.py
@@ -1,5 +1,8 @@
 full_name = input("inter your full name")
-number = input("inter your number")  #mobile number should be according to this formate (05*-***-****)
-yob = input("inter your year of birth")
-user_age = 2023 - ("yob")
-print ("user_age")
+number = input("inter your number 05*-***-****")  #mobile number should be according to this formate (05*-***-****)
+number = str
+year_of_birth = input("inter your year of birth")
+user_age = f"""2023 - {year_of_birth}"""
+print(full_name)
+print(number.split("-"))
+print(user_age)

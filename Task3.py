eaf3c328a9b2c7cd0b6790809fd6ee13c987c9c2
Nour-Abu-Foul <nourabufoul@gmail.com>
2023-08-02 16:44:34 +0300

    second commit

--- Task3.py
+++ Task3.py
@@ -1,21 +1,18 @@
-def add_function ():
-    number1 = int(input("inter number 1:"))
-    number2 = int (input("inter number 2:"))
-    total =  (number1 + number2)
+number1 = int(input("inter number 1:"))
+number2 = int (input("inter number 2:"))
+def add_function (number1,number2):
+    total =  number1 + number2
     return total
-add_function()
-
-def subtraction_function ( ):
-    number1 = int (input("inter number 1:"))
-    number2 = int (input("inter number 2:"))
-    total2 =  (number1 - number2)
+add_function(number1,number2)
+print (add_function(number1,number2))
+def subtraction_function (number1,number2):
+    total2 =  number1 - number2
     return total2
-subtraction_function ( )
-
-def multiplication_function ():
-    number1 = int (input("inter number 1:"))
-    number2 = int (input("inter number 2:"))
-    total3 = int (number1 * number2)
+subtraction_function (number1,number2)
+print (subtraction_function (number1,number2))
+def multiplication_function (number1,number2):
+    total3 =  number1 * number2
     return total3
-multiplication_function()
+multiplication_function(number1,number2)
 
+print (multiplication_function (number1,number2))

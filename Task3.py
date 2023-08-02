254cb1c265c9e944211766fe4792d201679fda93
Nour-Abu-Foul <nourabufoul@gmail.com>
2023-08-02 16:16:36 +0300

    new commit

---
+++ Task3.py
@@ -0,0 +1,14 @@
+def add_function ():
+    number1 = input("inter number 1:")
+    number2 = input("inter number 2:")
+    total = int (number1 + number2)
+    return total
+add_function()
+def subtraction_function ( ):
+    number1 = input("inter number 1:")
+    number2 = input("inter number 2:")
+    total2 = int (number1 + number2)
+    return total2
+subtraction_function ( )
+
+def multiplication_function

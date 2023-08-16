ebab24e1356e955b1acbb5b72aa63ea2c28e9ed0
Nour-Abu-Foul <nourabufoul@gmail.com>
2023-08-16 13:01:10 +0300

    new commit

--- loop.py
+++ loop.py
@@ -26,32 +26,32 @@ while True :
     if selections == 1:
         num1 = float(input("inter first number "))
         num2 = float(input("inter second number "))
-    sum(num1=num1,num2=num2)
+        sum(num1=num1,num2=num2)
 
     elif selections == 2:
         num1 = float(input("inter first number "))
         num2 = float(input("inter second number "))
-    subtract(num1=num1,num2=num2)
+        subtract(num1=num1,num2=num2)
 
     elif selections == 3:
         num1 = float(input("inter first number "))
         num2 = float(input("inter second number "))
-    multiply(num1=num1,num2=num2)
+        multiply(num1=num1,num2=num2)
     elif selections == 4:
         num1 = float(input("inter first number "))
         num2 = float(input("inter second number "))
-    division(num1=num1,num2=num2)
+        division(num1=num1,num2=num2)
     elif selections == 5:
         b = float(input("inter the Triangle Base "))
         l = float(input("inter the Triangle length "))
-    triangle_area(base=b,length=l)
+        triangle_area(base=b,length=l)
     elif selections == 6:
         r = float(input("inter the Circle Radius "))
-    circlr_area(raduis=r)
+        circlr_area(radius=r)
     elif selections == 7:
         le = float(input("inter rectangle Length "))
         w = float(input("inter rectangle width "))
-    rectangle(length=le,width=w)
+        rectangle_area(Length=le,width=w)
     elif selections == 8:
         exit()
     else:

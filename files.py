50d666d95bdb95e32803efd4d61f2ab33ff259dc
Nour-Abu-Foul <nourabufoul@gmail.com>
2023-08-22 19:38:17 +0300

    new commit

--- files.py
+++ files.py
@@ -1,4 +1,9 @@
 with open('new_file.txt', "a") as file:
-    pruser_name = input("Enter your name: ")
+
+    user_name = input("Enter your name: ")
     user_age = input("Enter your age: ")
     date_of_birth = input("Enter your date of birth (YYYY-MM-DD): ")
+    user_info = f"""User Name: {user_name}" 
+    Age: {user_age}" 
+    Date of Birth: {date_of_birth}"""
+    file.write(user_info)
